278bcd6de4ce62aeb2ee1aa8e5a85db42c458486
walter rodrigues junior <walterrjr.86@gmail.com>
2022-03-08 21:32:17 -0300

    atualizando

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('site/style.css')}}">
 </head>
 <body>
+
+
     
+    <script src="{{asset('site/jquery.js')}}"></script>
+    <script src="{{asset('site/bootstrap.js')}}"></script>
 </body>
 </html>
